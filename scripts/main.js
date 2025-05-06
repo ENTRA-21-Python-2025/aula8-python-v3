b6e1562964cb723e99c98828d674ccee8ea1f1fd
Nicholas Macedo <nicholasmacedoo@gmail.com>
2025-05-06 20:27:11 -0300

    :sparkles: Meu primeiro commit

---
+++ scripts/main.js
@@ -0,0 +1,83 @@
+/** 
+ * Arrays e Objetos
+ */
+var frutas = [
+    "maça",
+    "banana",
+    "laranja",
+];
+
+// console.log(typeof frutas)
+
+var pessoa = {
+    nome: "Amora",
+    idade: 1,
+    email: "amora@sememail.com.br",
+    senha: 123456,
+    deAcordoTermos: false, // camelCase
+    // criadoEm: new Date(),
+    // "de-acordo-termos": ""
+}
+
+// pessoa.test = ""
+
+frutas.push("pêra")
+
+/** 
+ * Declarando uma função
+ */
+// function nomeDaFuncao(valor, indice) {
+//     // 
+//     // const test = prompt("Informe seu nome")
+//     alert("Valor passado: " + valor + " Indice:" + indice)
+// }
+
+// nomeDaFuncao("test", 0)
+
+// var novoArray = frutas.map(function(valor, indice) {
+//     console.log(indice, valor)
+//     return valor + " ta vendo"
+// })
+// frutas.map(function(valor, indice) {
+//     console.log(indice, valor)
+//     return valor + " ta vendo"
+// })
+
+// console.log(novoArray)
+
+// console.log(pessoa["nome"])
+// console.log(pessoa.email)
+// console.log(frutas)
+// console.log(pessoa.criadoEm)
+// console.log(pessoa["de-acordo-termos"])
+
+
+/** 
+ * Capturando elementos
+ */
+
+// var containerElemento = document.getElementById("container")
+var buttonElemento = document.querySelector("button")
+var inputElemento = document.querySelector("input")
+var listaElemento = document.querySelector("ul")
+// var itemElemento = document.querySelector("li")
+var tarefas = []
+// buttonElemento.innerHTML = "<span>teste</span>"
+// // buttonElemento.innerText = "<span>teste</span>"
+// console.log(itemElemento)
+
+buttonElemento.addEventListener("click", function() {
+    
+    let valorInput = inputElemento.value
+    
+    tarefas.push(valorInput)
+
+    inputElemento.value = ""
+
+    const itemElemento = document.createElement("li")
+    itemElemento.innerText = valorInput
+
+    listaElemento.appendChild(itemElemento)
+    // listaElemento.innerHTML = "<li class='list-group-item'>" + valorInput + "</li>"
+    console.log(tarefas)
+})
